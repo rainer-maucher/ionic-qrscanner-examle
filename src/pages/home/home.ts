8f2b80a08b6f8c96c5b7e3744708a14643fdae72
Rainer Maucher <rainer.maucher@gmail.com>
2017-06-09 15:04:50 +0200

    Add basic ionic qr scanner

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
+    // todo implement this
+    this.barcodeScanner.scan().then((barcodeData) => {
+      // Success! Barcode data is here
+      console.log(barcodeData)
+    }, (err) => {
+      // An error occurred
+    });
   }
-
 }
